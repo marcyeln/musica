3c9f9e51de5ff19a3f144a6dd6ae906fbdbb3b56
marcyeln <carmoaparecida20@gmail.com>
2020-02-28 22:08:46 -0300

    adicionei resposta dos exercicios

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace musica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("You're the coffee that I need in the morning");
+            Console.WriteLine("You're my sunshine in the rain when it's pouring "); 
+            Console.WriteLine("Won't you give yourself to me ");
+            Console.WriteLine("Won't you give yourself to me ");
+            Console.WriteLine("Give it all, oh ");
+            Console.WriteLine("I just wanna see ");
+            Console.WriteLine("I just wanna see how beautiful you are");
+            console.WriteLine("I know you're a star");
+            
+
+            
+
+        }   
+    }
+}
